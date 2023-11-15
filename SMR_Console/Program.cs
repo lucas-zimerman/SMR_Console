bb1b45f250f084c017155625c23a2ce2e3ec1ad6
lucas <lucas-zimerman1@hotmail.com>
2023-11-15 15:46:30 -0300

    Add project files.

---
+++ SMR_Console/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sentry;
+
+namespace SMR_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SentrySdk.Init(o => { o.Dsn = "https://eb18e953812b41c3aeb042e666fd3b5c@o447951.ingest.sentry.io/5428537"; o.Debug = true; });
+            SentrySdk.CaptureMessage("a");
+            Console.ReadKey();
+        }
+    }
+}
